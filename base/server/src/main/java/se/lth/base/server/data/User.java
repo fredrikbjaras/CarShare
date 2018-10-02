17dd09df7df6e431088ac82428b16f7a66ca0ee4
LukasForsberg <43639601+LukasForsberg@users.noreply.github.com>
2018-10-02 12:41:35 +0200

    Update User.java

--- base/server/src/main/java/se/lth/base/server/data/User.java
+++ base/server/src/main/java/se/lth/base/server/data/User.java
@@ -1,6 +1,9 @@
 package se.lth.base.server.data;
 
+import java.awt.Image;
+
 import java.security.Principal;
+
 
 public class User implements Principal {
 
@@ -9,12 +12,23 @@ public class User implements Principal {
     private final int id;
     private final Role role;
     private final String username;
+    private Image profilePicture;
+    private int phoneNr;
+    private String description;
+	private Boolean isAdmin;
 
-    public User(int id, Role role, String username) {
+
+    public User(int id,Role role, String userName) {
         this.id = id;
         this.role = role;
-        this.username = username;
-    }
+        this.username = userName;
+        if(role == Role.ADMIN ) {
+        	isAdmin = true;
+        }
+        else {
+        	isAdmin = false;
+        }
+   }
 
     public Role getRole() {
         return role;
@@ -28,4 +42,34 @@ public class User implements Principal {
     public String getName() {
         return username;
     }
+    
+    public Image getProfilePicture() {
+    	return profilePicture;
+    }
+    
+    public void setProfilePicture(Image im) {
+    	profilePicture = im;
+    }
+    
+    
+    public int getPhoneNr() {
+    	return phoneNr;
+    }
+    
+    public void setPhoneNr(int phoneNr) {
+    	this.phoneNr = phoneNr;
+    }
+    
+    public String getDescription() {
+    	return description;
+    }
+    
+    public void setDescription(String de) {
+    	description = de;
+    }
+    
+    public boolean getIsAdmin() {
+    	return isAdmin;
+    }
+    
 }
