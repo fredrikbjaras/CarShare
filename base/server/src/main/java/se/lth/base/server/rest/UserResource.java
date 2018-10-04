f8fc7a0c0148925a4f8bac90d512bb1fd8b57179
OskarVateman <43718808+OskarVateman@users.noreply.github.com>
2018-10-04 16:34:31 +0200

    Update UserResource.java

--- base/server/src/main/java/se/lth/base/server/rest/UserResource.java
+++ base/server/src/main/java/se/lth/base/server/rest/UserResource.java
@@ -82,14 +82,27 @@ public class UserResource {
         return Role.ALL_ROLES;
     }
 
+    /**
+     * @param credentials
+     * @return returns the user added
+     * 
+     * Ska userDao.addUser(...) ha credentials eller ej? Går ej att inputta lösenord om inte parametrarna i denna addUser ändras.
+     * Om userDao.addUser ger en DataAccessException, är inte det onödigt?
+     */
     @POST
     @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
     @RolesAllowed(Role.Names.ADMIN)
-    public User createUser(Credentials credentials) {
-        if (!credentials.hasPassword() || !credentials.validPassword()) {
-            throw new WebApplicationException("Password too short", Response.Status.BAD_REQUEST);
-        }
-        return userDao.addUser(credentials);
+    public User addUser(Credentials credentials) {
+    	try {
+    		if (userDao.getUserWithName(credentials.getUsername()).getUserName() == credentials.getUsername()) {
+    			throw new WebApplicationException("Username is taken", Response.Status.BAD_REQUEST);
+    		}
+    	} catch (DataAccessException e) {
+            if (!credentials.hasPassword() || !credentials.validPassword()) {
+                throw new WebApplicationException("Password too short", Response.Status.BAD_REQUEST);
+            }
+            return userDao.addUser(credentials);
+    	}
     }
 
     @Path("all")
@@ -100,12 +113,36 @@ public class UserResource {
         return userDao.getUsers();
     }
 
+    /**
+     * @param userId
+     * @return returns the user with the userId
+     * 
+     * En ny user med konstruktorn (-1, null, null, phoneNr, false) returneras när man enbart vill returnera telefonnumret.
+     * En metod checkIfOnSameActiveRoute bör finnas med i någon klass, Route?, User? för att kolla om två users befinner/befann sig på
+     * samma route, detta kan vara värt då detta behöver kollas i flera metoder i *-Resource-klasserna.
+     * Bör passagerarna kunna få andra passagerares telefonnummer?
+    */
     @Path("{id}")
     @GET
-    @RolesAllowed(Role.Names.ADMIN)
+    @PermitAll
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public User getUser(@PathParam("id") int userId) {
-        return userDao.getUser(userId);
+    	try {
+    		if(user.getIsAdmin() || user.getUserID() == userId) {
+            		return userDao.getUser(userId);
+        	} else if (user.checkIfOnSameActiveRoute(userId)) {
+            		return new User(-1, null, null, userDao.getUser(userId).getPhoneNr(), false);
+        	} else {
+        		throw new WebApplicationException("Requirements not met", Response.Status.BAD_REQUEST);
+        	}
+    	} catch (DataAccessException e) {
+    		//Man kanske inte behöver if-else-satserna i catch-blocket.
+    		if(user.getIsAdmin() || user.getUserID() == userId || user.checkIfOnSameActiveRoute(userId)) {
+    			throw new WebApplicationException("Requirements met but User not found", Response.Status.BAD_REQUEST);
+    		} else {
+    			throw new WebApplicationException("Requirements not met", Response.Status.BAD_REQUEST);
+    		}
+    	}
     }
 
     @Path("{id}")
