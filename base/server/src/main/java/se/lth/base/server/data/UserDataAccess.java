5ca68d26edb2feb29de22291d183f7adad26ec7c
LukasForsberg <43639601+LukasForsberg@users.noreply.github.com>
2018-10-08 14:49:14 +0200

    Update UserDataAccess.java
    test

--- base/server/src/main/java/se/lth/base/server/data/UserDataAccess.java
+++ base/server/src/main/java/se/lth/base/server/data/UserDataAccess.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.function.Supplier;
 
- /* @author Jonathan Jakobsson
+ /* @author Jonathan Jakobsson and Lukas Forsberg
  * @see DataAccess
  */
 public class UserDataAccess extends DataAccess<User> {
