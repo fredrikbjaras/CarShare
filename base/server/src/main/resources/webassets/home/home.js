208c5e93360d977985e4f33f324df73a588d38bd
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2018-10-08 14:59:54 +0200

    Empty home.js

--- base/server/src/main/resources/webassets/home/home.js
+++ base/server/src/main/resources/webassets/home/home.js
@@ -1,46 +1,21 @@
 var base = base || {};
 base.homeController = function() {
-	// List of all foo data, will be useful to have when update functionality is added.
     var model = [];
 
     var view = {
-        // Creates HTML for each foo in model
         render: function() {
-//            model.forEach(d => view.renderPart(d));
         },
-        // Creates HTML for foo parameter and adds it to the parent of the template
-        renderPart: function(foo) {
-            // Gets a reference to the template.
-            // A template element is a special element used only to add dynamic content multiple times.
-            // See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
-//            var t = view.template();
-//            view.update(t.content.querySelector('tr'), foo);
-//            var clone = document.importNode(t.content, true);
-//            t.parentElement.appendChild(clone);
-        },
-        // Update a single table row to display a foo
-        update: function(trElement, foo) {
-//            var tds = trElement.children;
-//            tds[0].textContent = foo.payload;
-//            var d = foo.createdDate;
-//            tds[1].textContent = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
-        }
 
-//        template: function() {
-//            return document.getElementById('createdRoutes-template');
-//        }
+        update: function(trElement, foo) {
+        }
     };
 
 
 
     var controller = {
 		load: function() {
-//            base.rest.getFoos().then(function(foos) {
-//                model = foos;
-//                view.render();
-//            });
+            view.render();
         }
     };
-    //window.initOnLoad();
     return controller;
 };
