febbaf6f4cb2db40fa9b092edf47a39dd25e7755
OskarVateman <43718808+OskarVateman@users.noreply.github.com>
2018-10-09 16:23:02 +0200

    Update RouteFilter.java

--- base/server/src/main/java/se/lth/base/server/data/RouteFilter.java
+++ base/server/src/main/java/se/lth/base/server/data/RouteFilter.java
@@ -5,15 +5,15 @@ import java.sql.Timestamp;
 public class RouteFilter {
 	
 		private final int driverID;
-		private final String orgin;
+		private final String location;
 		private final String destination;
 		private final Timestamp departureTime;
 		private final Timestamp arrivalTime;
 	
-		public RouteFilter(int driverID, String orgin, String destination,String departureTime, String arrivalTime) {
+		public RouteFilter(int driverID, String location, String destination,String departureTime, String arrivalTime) {
 			
 			this.driverID = driverID;
-			this.orgin = orgin;
+			this.location = location;
 			this.destination = destination;
 			this.departureTime = Timestamp.valueOf(departureTime);
 			this.arrivalTime = Timestamp.valueOf(arrivalTime);
@@ -23,8 +23,8 @@ public class RouteFilter {
 			return driverID;
 		}
 		
-		public String getOrgin() {
-			return orgin;
+		public String getLocation() {
+			return location;
 		}
 
 		public String getDestination() {
@@ -38,6 +38,30 @@ public class RouteFilter {
 			return arrivalTime;
 		}
 
+	/*
+	 * 1 if driverID 
+	 * 2 if origin
+	 * 3 if destination
+	 * 4 if departureTime
+	 * 5 if arrivalTime
+	 * 0 if nothing
+	 */
+	public int getFilter() {
+		if(driverID != 0) {// could be a empty String
+			return 1; 
+		}
+		else if (location != null) {
+			return 2;
+		}
+		else if(destination != null) {
+			return 3;
+		} else if (departureTime != null) {
+			return 4;
+		} else if (arrivalTime != null) {
+			return 5;
+		}
+		return 0;
+	}
 
 	
 
