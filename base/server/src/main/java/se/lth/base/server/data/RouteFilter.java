28617ad3802fc1ccca8bbc3092bdc5273fe036bc
OskarVateman <43718808+OskarVateman@users.noreply.github.com>
2018-10-15 15:45:11 +0200

    Update RouteFilter.java

--- base/server/src/main/java/se/lth/base/server/data/RouteFilter.java
+++ base/server/src/main/java/se/lth/base/server/data/RouteFilter.java
@@ -5,14 +5,15 @@ import java.sql.Timestamp;
 public class RouteFilter {
 	
 		private final String driverUserName;
+		private final String passengerUserName;
 		private final String location;
 		private final String destination;
 		private final String departureTime;
 		private final String arrivalTime;
 	
-		public RouteFilter(String driverUserName, String location, String destination,String departureTime, String arrivalTime) {
-			
+		public RouteFilter(String driverUserName, String passengerUserName, String location, String destination,String departureTime, String arrivalTime) {
 			this.driverUserName = driverUserName;
+			this.passengerUserName = passengerUserName;
 			this.location = location;
 			this.destination = destination;
 			this.departureTime = departureTime;
@@ -21,6 +22,10 @@ public class RouteFilter {
 		
 		public String getDriverUserName() {
 			return driverUserName;
+		}
+	
+		public String getPassengerUserName() {
+			return passengerUserName;
 		}
 		
 		public String getLocation() {
@@ -39,26 +44,27 @@ public class RouteFilter {
 		}
 
 	/*
-	 * 1 if driverID 
-	 * 2 if origin
-	 * 3 if destination
-	 * 4 if departureTime
-	 * 5 if arrivalTime
+	 * 1 if driverID (driverUserName)
+	 * 2 if passengerUserName 
+	 * 3 if origin
+	 * 4 if destination
+	 * 5 if departureTime
+	 * 6 if arrivalTime
 	 * 0 if nothing
 	 */
 	public int getFilter() {
 		if(driverUserName != null && !driverUserName.equals("")) {// could be a empty String
 			return 1; 
-		}
-		else if (location != null) {
+		} else if (passengerUserName != null && !passengerUserName.equals("")) {
 			return 2;
-		}
-		else if(destination != null) {
+		} else if (location != null) {
 			return 3;
-		} else if (departureTime != null) {
+		} else if(destination != null) {
 			return 4;
-		} else if (arrivalTime != null) {
+		} else if (departureTime != null) {
 			return 5;
+		} else if (arrivalTime != null) {
+			return 6;
 		}
 		return 0;
 	}
